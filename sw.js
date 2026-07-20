1d3ad530565ebfed2bb1769033cd6fca5bbd42bc
Giovanna <104276166+ohinna@users.noreply.github.com>
2026-07-19 21:09:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Bazaar — Service Worker v3625e5e8
-const CACHE_NAME = 'bazaar-3625e5e8';
+// Bazaar — Service Worker vded370eb
+const CACHE_NAME = 'bazaar-ded370eb';
 const FILES = ['/ir-atras/', '/ir-atras/index.html'];
 
 self.addEventListener('install', e => {
